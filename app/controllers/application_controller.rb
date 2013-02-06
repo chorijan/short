726da1a540a1ccca10064bba0f669a80a960c8c5
Dean Perry <dean@deanperry.net>
2013-02-06 13:10:07 +0000

    current_site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery
+
+  def current_site
+    @site ||= Site.find_by_url(request.host)
+  end
+
+  helper_method :current_site
+
 end
