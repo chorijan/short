b76e5a384cdeb9d194d55fc392c08be0bc8542f5
Dean Perry <dean@deanperry.net>
2013-02-06 14:57:19 +0000

    removed sorting

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,7 +1,5 @@
 class Admin::SitesController < ApplicationController
 
-  helper_method :sort_column, :sort_direction
-
   def index
     @sites = Site.all
   end
@@ -45,16 +43,6 @@ class Admin::SitesController < ApplicationController
     @site.destroy
 
     redirect_to admin_sites_url
-  end
-
-  private
-  
-  def sort_column
-    @site.urls.column_names.include?(params[:sort]) ? params[:sort] : "id"
-  end
-  
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
