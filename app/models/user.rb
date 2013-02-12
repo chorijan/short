ba76b1077c1f36a66806b8383972fc260e6bdb67
Dean Perry <dean@deanperry.net>
2013-02-12 21:05:01 +0000

    more on user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest
+  
+  has_secure_password
+  
+	attr_accessible :email, :password, :password_confirmation
+  
+	validates_uniqueness_of :email
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206174308) do
+ActiveRecord::Schema.define(:version => 20130212203251) do
 
   create_table "clicks", :force => true do |t|
     t.integer  "url_id"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(:version => 20130206174308) do
     t.datetime "updated_at", :null => false
     t.integer  "old_clicks"
     t.string   "title"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
